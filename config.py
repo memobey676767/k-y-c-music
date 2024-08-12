8f0324237e7e6e9825ebee80bbe1a82e5ffbd491
KUMSAL MÜZİK <kurulummm0@gmail.com>
2024-08-12 06:52:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mamakli06/mamaklimusic1",
+    "https://github.com/Rq7mg/k-y-c-mus",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
